96de257ab736ca1537e9075478d10312af22920b
ledass <nistalieocs@gmail.com>
2025-07-19 12:45:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -198,7 +198,7 @@ async def start(client, message):
             ]]      
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_video(
-            video="https://envs.sh/FE1.mp4",
+            video="https://envs.sh/_O0.mp4",
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
@@ -231,7 +231,7 @@ async def start(client, message):
             ]]      
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_video(
-            video="https://envs.sh/FE1.mp4",
+            video="https://envs.sh/_O0.mp4",
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
